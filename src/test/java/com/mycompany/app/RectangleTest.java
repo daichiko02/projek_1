5a85cc9cfda26bb9bade2d95ddcc9eea23da54a1
SEPTIAN SOLIHIN <20058713@MAC1CMJHCDCHIP.local>
2020-04-04 14:08:47 +0700

    branch septian use hotkey opc + cmd + l to make up the code

--- src/test/java/com/mycompany/app/RectangleTest.java
+++ src/test/java/com/mycompany/app/RectangleTest.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RectangleTest {
     @Test
-    void area_shouldReturn12WhenInputLengthIs4AndWidthIs3(){
-        Rectangle rectangle = new Rectangle(4,3);
+    void area_shouldReturn12WhenInputLengthIs4AndWidthIs3() {
+        Rectangle rectangle = new Rectangle(4, 3);
 
         int result = rectangle.area();
 
-        assertEquals(12,result);
+        assertEquals(12, result);
     }
 }
